050cf034dd9cae36d016f8408d71f685c49588c1
Michael Adler <Michael.Adler@intel.com>
2015-12-18 16:11:25 -0500

    Clean up

--- devicetypes/michael-adler/generic-image-capture.src/generic-image-capture.groovy
+++ devicetypes/michael-adler/generic-image-capture.src/generic-image-capture.groovy
@@ -22,94 +22,94 @@
 //
 
 metadata {
-	definition (name: "Generic Image Capture", namespace: "michael-adler", author: "Michael Adler") {
-		capability "Image Capture"
-		capability "Sensor"
-	}
+    definition (name: "Generic Image Capture", namespace: "michael-adler", author: "Michael Adler") {
+        capability "Image Capture"
+        capability "Sensor"
+    }
 
-	simulator {
-		// TODO: define status and reply messages here
-	}
+    simulator {
+        // TODO: define status and reply messages here
+    }
 
-	preferences {
- 		input("CameraIP", "string", title:"Camera IP Address", description: "Please enter your camera's IP Address", required: true, displayDuringSetup: true)
-		input("CameraPort", "string", title:"Camera Port", description: "Please enter your camera's Port", defaultValue: 80 , required: true, displayDuringSetup: true)
-		input("CameraPath", "string", title:"Camera Path to Image", description: "Please enter the path to the image", defaultValue: "/SnapshotJPEG?Resolution=640x480&Quality=Clarity", required: true, displayDuringSetup: true)
-		input("CameraPostGet", "string", title:"Does Camera use a Post or Get, normally Get?", description: "Please choose if the camera uses a POST or a GET command to retreive the image", defaultValue: "GET", displayDuringSetup: true)
-		input("CameraAuth", "bool", title:"Does Camera require User Auth?", description: "Please choose if the camera requires authentication (only basic is supported)", defaultValue: true, displayDuringSetup: true)
- 		input "CameraUsername", "text", title: "Camera Username", description: "Camera Username", required: true
-		input "CameraPassword", "password", title: "Camera Password", description: "Camera Password", required: true
-	}
+    preferences {
+        input("CameraIP", "string", title:"Camera IP Address", description: "Please enter your camera's IP Address", required: true, displayDuringSetup: true)
+        input("CameraPort", "string", title:"Camera Port", description: "Please enter your camera's Port", defaultValue: 80 , required: true, displayDuringSetup: true)
+        input("CameraPath", "string", title:"Camera Path to Image", description: "Please enter the path to the image", defaultValue: "/SnapshotJPEG?Resolution=640x480&Quality=Clarity", required: true, displayDuringSetup: true)
+        input("CameraPostGet", "string", title:"Does Camera use a Post or Get, normally Get?", description: "Please choose if the camera uses a POST or a GET command to retreive the image", defaultValue: "GET", displayDuringSetup: true)
+        input("CameraAuth", "bool", title:"Does Camera require User Auth?", description: "Please choose if the camera requires authentication (only basic is supported)", defaultValue: true, displayDuringSetup: true)
+        input "CameraUsername", "text", title: "Camera Username", description: "Camera Username", required: true
+        input "CameraPassword", "password", title: "Camera Password", description: "Camera Password", required: true
+    }
 
-	tiles {
-		standardTile("image", "device.image", width: 1, height: 1, canChangeIcon: false, inactiveLabel: true, canChangeBackground: true) {
-			state "default", label: "", action: "", icon: "st.camera.dropcam-centered", backgroundColor: "#FFFFFF"
-		}
+    tiles {
+        standardTile("image", "device.image", width: 1, height: 1, canChangeIcon: false, inactiveLabel: true, canChangeBackground: true) {
+            state "default", label: "", action: "", icon: "st.camera.dropcam-centered", backgroundColor: "#FFFFFF"
+        }
 
-		carouselTile("cameraDetails", "device.image", width: 3, height: 2) { }
+        carouselTile("cameraDetails", "device.image", width: 3, height: 2) { }
 
-		standardTile("take", "device.image", width: 1, height: 1, canChangeIcon: false, inactiveLabel: true, canChangeBackground: false) {
-			state "take", label: "Take", action: "Image Capture.take", icon: "st.camera.dropcam", backgroundColor: "#FFFFFF", nextState:"taking"
-			state "taking", label:'Taking', action: "", icon: "st.camera.dropcam", backgroundColor: "#53a7c0"
-			state "image", label: "Take", action: "Image Capture.take", icon: "st.camera.dropcam", backgroundColor: "#FFFFFF", nextState:"taking"
-		}
+        standardTile("take", "device.image", width: 1, height: 1, canChangeIcon: false, inactiveLabel: true, canChangeBackground: false) {
+            state "take", label: "Take", action: "Image Capture.take", icon: "st.camera.dropcam", backgroundColor: "#FFFFFF", nextState:"taking"
+            state "taking", label:'Taking', action: "", icon: "st.camera.dropcam", backgroundColor: "#53a7c0"
+            state "image", label: "Take", action: "Image Capture.take", icon: "st.camera.dropcam", backgroundColor: "#FFFFFF", nextState:"taking"
+        }
 
-		main "image"
-		details(["cameraDetails", "take"])
-	}
+        main "image"
+        details(["cameraDetails", "take"])
+    }
 }
 
 // parse events into attributes
 def parse(String description) {
-	log.debug "Parsing '${description}'"
+    log.debug "Parsing '${description}'"
 
-	def map = stringToMap(description)
-	log.debug map
+    def map = stringToMap(description)
+    log.debug map
 
-	def result = []
+    def result = []
 
-	if (map.bucket && map.key)
-	{ //got a s3 pointer
-		putImageInS3(map)
-	}
-	else if (map.headers && map.body)
-	{ //got device info response
-    	def body = new String(descMap["body"].decodeBase64())
+    if (map.bucket && map.key)
+    { //got a s3 pointer
+        putImageInS3(map)
+    }
+    else if (map.headers && map.body)
+    { //got device info response
+        def body = new String(descMap["body"].decodeBase64())
         log.debug "Body: ${body}"
-	}
+    }
 
-	result
+    result
 }
 
 def putImageInS3(map) {
 
-	def s3ObjectContent
+    def s3ObjectContent
 
-	try {
-		def imageBytes = getS3Object(map.bucket, map.key + ".jpg")
+    try {
+        def imageBytes = getS3Object(map.bucket, map.key + ".jpg")
 
-		if(imageBytes)
-		{
-			s3ObjectContent = imageBytes.getObjectContent()
-			def bytes = new ByteArrayInputStream(s3ObjectContent.bytes)
-			storeImage(getPictureName(), bytes)
-		}
-	}
-	catch(Exception e) {
-		log.error e
-	}
-	finally {
-		//explicitly close the stream
-		if (s3ObjectContent) { s3ObjectContent.close() }
-	}
+        if(imageBytes)
+        {
+            s3ObjectContent = imageBytes.getObjectContent()
+            def bytes = new ByteArrayInputStream(s3ObjectContent.bytes)
+            storeImage(getPictureName(), bytes)
+        }
+    }
+    catch(Exception e) {
+        log.error e
+    }
+    finally {
+        //explicitly close the stream
+        if (s3ObjectContent) { s3ObjectContent.close() }
+    }
 }
 
 // handle commands
 def take() {
-	log.debug "Executing 'take'"
+    log.debug "Executing 'take'"
 
-	def userpassascii = "${CameraUsername}:${CameraPassword}"
-	def userpass = "Basic " + userpassascii.encodeAsBase64().toString()
+    def userpassascii = "${CameraUsername}:${CameraPassword}"
+    def userpass = "Basic " + userpassascii.encodeAsBase64().toString()
     def host = CameraIP 
     def hosthex = convertIPtoHex(host)
     def porthex = convertPortToHex(CameraPort)
@@ -117,52 +117,48 @@ def take() {
     
     log.debug "The device id configured is: $device.deviceNetworkId"
     
-	def path = CameraPath
-    log.debug "path is: $path"
+    def path = CameraPath
     
     def headers = [:] 
     headers.put("HOST", "$host:$CameraPort")
-	if (CameraAuth) {
-		headers.put("Authorization", userpass)
+    if (CameraAuth) {
+        headers.put("Authorization", userpass)
     }
     
-    log.debug "The Header is $headers"
+    def method = CameraPostGet
     
-    def method = "GET"
-    log.debug "Thex method is $method"
-    
-//    try {
+    try {
         def hubAction = new physicalgraph.device.HubAction(
-    	    method: method,
-    	    path: path,
-    	    headers: headers
+            method: method,
+            path: path,
+            headers: headers
             )
-        	
-    	hubAction.options = [outputMsgToS3:true]
-    	log.debug hubAction
-    	return hubAction
-//    }
-//    catch (Exception e) {
-//    	log.debug "Hit Exception $e on $hubAction"
-//    }
+            
+        hubAction.options = [outputMsgToS3:true]
+        log.debug hubAction
+        return hubAction
+    }
+    catch (Exception e) {
+        log.debug "Hit Exception $e on $hubAction"
+    }
 }
 
 //helper methods
 private getPictureName() {
-	def pictureUuid = java.util.UUID.randomUUID().toString().replaceAll('-', '')
-	return device.deviceNetworkId + "_$pictureUuid" + ".jpg"
+    def pictureUuid = java.util.UUID.randomUUID().toString().replaceAll('-', '')
+    return device.deviceNetworkId + "_$pictureUuid" + ".jpg"
 }
 
 private getUsername() {
-	settings.username
+    settings.username
 }
 
 private getPassword() {
-	settings.password
+    settings.password
 }
 
 private String convertHexToIP(hex) {
-	[convertHexToInt(hex[0..1]),convertHexToInt(hex[2..3]),convertHexToInt(hex[4..5]),convertHexToInt(hex[6..7])].join(".")
+    [convertHexToInt(hex[0..1]),convertHexToInt(hex[2..3]),convertHexToInt(hex[4..5]),convertHexToInt(hex[6..7])].join(".")
 }
 
 private String convertIPtoHex(ipAddress) { 
@@ -173,11 +169,11 @@ private String convertIPtoHex(ipAddress) {
 }
 
 private String convertPortToHex(port) {
-	String hexport = port.toString().format( '%04x', port.toInteger() )
+    String hexport = port.toString().format( '%04x', port.toInteger() )
     log.debug hexport
     return hexport
 }
 
 private Integer convertHexToInt(hex) {
-	Integer.parseInt(hex,16)
+    Integer.parseInt(hex,16)
 }
