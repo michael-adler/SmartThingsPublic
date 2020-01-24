60a15d0375c9dab74470e5f8a41b2bf34808508f
Michael Adler <Michael.Adler@intel.com>
2020-01-24 15:45:51 -0500

    Make switched presence sensor work better with v3.

--- devicetypes/michael-adler/switched-presence-sensor.src/switched-presence-sensor.groovy
+++ devicetypes/michael-adler/switched-presence-sensor.src/switched-presence-sensor.groovy
@@ -1,7 +1,7 @@
 /**
  *  Switched Presence Sensor
  *
- *  Copyright 2015 Michael Adler
+ *  Copyright 2020 Michael Adler
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -20,95 +20,118 @@
 //
 
 metadata {
-	definition (name: "Switched Presence Sensor", namespace: "michael-adler", author: "Michael Adler") {
-		capability "Presence Sensor"
-		capability "Switch"
-	}
+    definition (name: "Switched Presence Sensor", namespace: "michael-adler", author: "Michael Adler") {
+        capability "Presence Sensor"
+        capability "Switch"
+        capability "Actuator"
+        capability "Sensor"
+        capability "Health Check"
+    }
 
-	simulator {
-		status "present": "presence: 1"
-		status "not present": "presence: 0"
-	}
+    simulator {
+        status "present": "presence: 1"
+        status "not present": "presence: 0"
+    }
 
-	tiles {
-		standardTile("presence", "device.presence", width: 2, height: 2, canChangeBackground: true) {
-			state("not present", labelIcon:"st.presence.tile.not-present", backgroundColor:"#ffffff")
-			state("present", labelIcon:"st.presence.tile.present", backgroundColor:"#53a7c0")
-		}
-    	standardTile("button", "device.switch", width: 2, height: 2, canChangeIcon: false) {
-			state "off", label: 'Off', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff", nextState: "on"
-			state "on", label: 'On', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#79b821", nextState: "off"
-		}      
-		main "presence"
-		details(["button", "presence"])
-	}
+    tiles {
+        standardTile("presence", "device.presence", width: 2, height: 2, canChangeBackground: true) {
+            state("not present", labelIcon:"st.presence.tile.not-present", backgroundColor:"#ffffff")
+            state("present", labelIcon:"st.presence.tile.present", backgroundColor:"#00A0DC")
+        }
+        standardTile("button", "device.switch", width: 2, height: 2, canChangeIcon: false) {
+            state("off", label: 'Away', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff", nextState: "on")
+            state("on", label: 'Present', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#00A0DC", nextState: "off")
+        }
+        main "presence"
+        details(["button", "presence"])
+    }
 }
 
 // parse events into attributes
 def parse(String description) {
-	log.debug "Parsing '${description}'"
+    log.debug "Parsing '${description}'"
 
-	def name = parseName(description)
-	def value = parseValue(description)
-	def linkText = getLinkText(device)
-	def descriptionText = parseDescriptionText(linkText, value, description)
-	def handlerName = getState(value)
-	def isStateChange = isStateChange(device, name, value)
+    def name = parseName(description)
+    def value = parseValue(description)
+    def linkText = getLinkText(device)
+    def descriptionText = parseDescriptionText(linkText, value, description)
+    def handlerName = getState(value)
+    def isStateChange = isStateChange(device, name, value)
 
-	def results = [
-		name: name,
-		value: value,
-		unit: null,
-		linkText: linkText,
-		descriptionText: descriptionText,
-		handlerName: handlerName,
-		isStateChange: isStateChange,
-		displayed: displayed(description, isStateChange)
-		]
+    def results = [
+        name: name,
+        value: value,
+        unit: null,
+        linkText: linkText,
+        descriptionText: descriptionText,
+        handlerName: handlerName,
+        isStateChange: isStateChange,
+        displayed: displayed(description, isStateChange)
+        ]
 
-	log.debug "Parse returned $results.descriptionText"
-	return results
+    log.debug "Parse returned $results.descriptionText"
+    return results
+}
+
+def installed() {
+    log.trace "Executing 'installed'"
+    off()
+    initialize()
+}
+
+def updated() {
+    log.trace "Executing 'updated'"
+    initialize()
+}
+
+def initialize() {
+    log.trace "Executing 'initialize'"
+    sendEvent(name: "DeviceWatch-DeviceStatus", value: "online")
+    sendEvent(name: "healthStatus", value: "online")
+    sendEvent(name: "DeviceWatch-Enroll", value: [protocol: "cloud", scheme:"untracked"].encodeAsJson(), displayed: false)
 }
 
 // handle commands
 def on() {
-	log.debug "Executing 'on'"
-	sendEvent(displayed: true, isStateChange: true, name: "presence", value: "present", descriptionText: "$device.displayName is present")
+    log.debug "Executing 'on'"
+    sendEvent(displayed: true, isStateChange: true, name: "presence", value: "present", descriptionText: "$device.displayName is present")
+    sendEvent(name: "switch", value: "on")
 }
 
 def off() {
-	log.debug "Executing 'off'"
-	sendEvent(displayed: true, isStateChange: true, name: "presence", value: "not present", descriptionText: "$device.displayName is not present")
+    log.debug "Executing 'off'"
+    sendEvent(displayed: true, isStateChange: true, name: "presence", value: "not present", descriptionText: "$device.displayName is not present")
+    sendEvent(name: "switch", value: "off")
 }
 
 private String parseName(String description) {
-	if (description?.startsWith("presence: ")) {
-		return "presence"
-	}
-	null
+    if (description?.startsWith("presence: ")) {
+        return "presence"
+    }
+    null
 }
 
 private String parseValue(String description) {
-	log.debug "Executing 'parseValue' (${description})"
-	switch (description) {
-		case "presence: 1": return "present"
-		case "presence: 0": return "not present"
-		default: return description
-	}
+    log.debug "Executing 'parseValue' (${description})"
+    switch (description) {
+        case "presence: 1": return "present"
+        case "presence: 0": return "not present"
+        default: return description
+    }
 }
 
 private parseDescriptionText(String linkText, String value, String description) {
-	switch (value) {
-		case "present": return "$linkText has arrived"
-		case "not present": return "$linkText has left"
-		default: return value
-	}
+    switch (value) {
+        case "present": return "$linkText has arrived"
+        case "not present": return "$linkText has left"
+        default: return value
+    }
 }
 
 private getState(String value) {
-	switch (value) {
-		case "present": return "arrived"
-		case "not present": return "left"
-		default: return value
-	}
+    switch (value) {
+        case "present": return "arrived"
+        case "not present": return "left"
+        default: return value
+    }
 }
